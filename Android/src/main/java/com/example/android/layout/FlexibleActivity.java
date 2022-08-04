9a6619e72f9d324ab666fae75d0ac8e490849b82
leizeling <1072113944@qq.com>
2022-08-04 20:39:16 +0800

    FlexboxLayout样例

---
+++ Android/src/main/java/com/example/android/layout/FlexibleActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.R;
+
+public class FlexibleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flexible);
+        FlexboxLayout fl =
+    }
+}
